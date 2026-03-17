db4ac8056647d3e21ec395d61b38332dc27e9359
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2026-03-17 23:29:43 +0100

    fix: add some env vars for the prod setup

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,8 +5,11 @@ from decouple import config
 
 from .base import *
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = []
+SITE_ORIGIN = os.environ.get("SITE_ORIGIN", "")
+FRONT_SITE_ORIGIN = os.environ.get("FRONT_SITE_ORIGIN", "")
+
+ALLOWED_HOSTS = [SITE_ORIGIN, FRONT_SITE_ORIGIN]
+CSRF_TRUSTED_ORIGINS = [f"https://{SITE_ORIGIN}", f"https://{FRONT_SITE_ORIGIN}"]
 
 DATABASES = {
     "default": dj_database_url.config(default="sqlite:///" + os.path.join("db.sqlite3"))
@@ -20,7 +23,10 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3080",
-#     "http://127.0.0.1:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    f"https://{SITE_ORIGIN}",
+    f"https://{FRONT_SITE_ORIGIN}",
+]
