7a3f733dbecbb00423b39c960ddcc9dd0de03b5a
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2026-05-02 12:31:52 +0100

    fix(config): use `decouple` to read `.env` values

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,11 +5,18 @@ from decouple import config
 
 from .base import *
 
-SITE_ORIGIN = os.environ.get("SITE_ORIGIN", "")
-FRONT_SITE_ORIGIN = os.environ.get("FRONT_SITE_ORIGIN", "")
+SITE_ORIGIN = config("SITE_ORIGIN", cast=str, default="")
+FRONT_SITE_ORIGIN = config("FRONT_SITE_ORIGIN", cast=str, default="")
+
+if not SITE_ORIGIN or not FRONT_SITE_ORIGIN:
+    raise RuntimeError("SITE_ORIGIN and FRONT_SITE_ORIGIN not found")
 
 ALLOWED_HOSTS = [SITE_ORIGIN, FRONT_SITE_ORIGIN]
-CSRF_TRUSTED_ORIGINS = [f"https://{SITE_ORIGIN}", f"https://{FRONT_SITE_ORIGIN}"]
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{SITE_ORIGIN}",
+    f"https://{FRONT_SITE_ORIGIN}",
+    "http://localhost",
+]
 
 DATABASES = {
     "default": dj_database_url.config(default="sqlite:///" + os.path.join("db.sqlite3"))
